94436325c228b44991ff09c8d575ed55bba70494
Saeed99Madi <saed.zooro@gmail.com>
2022-07-27 14:30:39 +0300

    create update function with test cases related to #29

--- js/logic/localStorage.js
+++ js/logic/localStorage.js
@@ -1,18 +1,26 @@
 // Pure functions
-function addToArr(arr,obj){
-  arr = [...arr,obj]
-  return arr
+function addToArr(arr, obj) {
+  arr = [...arr, obj];
+  return arr;
 }
-function removeFromArr(arr,id){
-  arr = [...arr]
+function removeFromArr(arr, id) {
+  arr = [...arr];
   arr.forEach((e, index) => {
     if (e.id === id) {
       arr.splice(index, 1);
     }
   });
-  return arr 
+  return arr;
 }
-
+function updateArr(arr, id, obj) {
+  arr = [...arr];
+  arr.forEach((e, index) => {
+    if (e.id === id) {
+      arr[index] = { ...obj, id };
+    }
+  });
+  return arr;
+}
 
 function getItem(key) {
   let arr = localStorage.getItem(key);
@@ -26,31 +34,27 @@ function setItem(key, arr) {
 function addItemTo(key, obj) {
   let arr = localStorage.getItem(key);
   arr = JSON.parse(arr);
-  arr = addToArr(arr,obj)
+  arr = addToArr(arr, obj);
   arr = JSON.stringify(arr);
   localStorage.setItem(key, arr);
 }
 function removeItemFrom(key, id) {
   let arr = localStorage.getItem(key);
   arr = JSON.parse(arr);
-  arr = removeFromArr(arr,id)
+  arr = removeFromArr(arr, id);
   arr = JSON.stringify(arr);
   localStorage.setItem(key, arr);
 }
 
-module.exports = {
-  addToArr ,
-  removeFromArr , 
-
-}
 function updateItemFrom(key, id, obj) {
   let arr = localStorage.getItem(key);
   arr = JSON.parse(arr);
-  arr.forEach((e, index) => {
-    if (e.id === id) {
-      arr[index] = { ...obj, id };
-    }
-  });
+  arr = updateArr(arr, id, obj);
   arr = JSON.stringify(arr);
   localStorage.setItem(key, arr);
 }
+module.exports = {
+  addToArr,
+  removeFromArr,
+  updateArr,
+};
