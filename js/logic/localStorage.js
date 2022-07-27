83221b24ca72af6873b0ec7a6aa8c7ba36ff1da5
Saeed99Madi <saed.zooro@gmail.com>
2022-07-27 12:07:05 +0300

    write update and get functions from localstorage related to #22

--- js/logic/localStorage.js
+++ js/logic/localStorage.js
@@ -1,3 +1,8 @@
+function getItem(key) {
+  let arr = localStorage.getItem(key);
+  arr = JSON.parse(arr);
+  return arr;
+}
 function setItem(key, arr) {
   arr = JSON.stringify(arr);
   localStorage.setItem(key, arr);
@@ -15,6 +20,18 @@ function removeItemFrom(key, id) {
   arr.forEach((e, index) => {
     if (e.id === id) {
       arr.splice(index, 1);
+    }
+  });
+  arr = JSON.stringify(arr);
+  localStorage.setItem(key, arr);
+}
+
+function updateItemFrom(key, id, obj) {
+  let arr = localStorage.getItem(key);
+  arr = JSON.parse(arr);
+  arr.forEach((e, index) => {
+    if (e.id === id) {
+      arr[index] = { ...obj, id };
     }
   });
   arr = JSON.stringify(arr);
