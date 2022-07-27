dc04068da731b433e09731182d00b578438f9ff3
SaifHayek <seefhayek4@gmail.com>
2022-07-27 13:37:00 +0300

    edit local storage functions and make test on them realtaed #17

--- js/logic/localStorage.js
+++ js/logic/localStorage.js
@@ -1,3 +1,19 @@
+// Pure functions
+function addToArr(arr,obj){
+  arr = [...arr,obj]
+  return arr
+}
+function removeFromArr(arr,id){
+  arr = [...arr]
+  arr.forEach((e, index) => {
+    if (e.id === id) {
+      arr.splice(index, 1);
+    }
+  });
+  return arr 
+}
+
+
 function setItem(key, arr) {
   arr = JSON.stringify(arr);
   localStorage.setItem(key, arr);
@@ -5,18 +21,20 @@ function setItem(key, arr) {
 function addItemTo(key, obj) {
   let arr = localStorage.getItem(key);
   arr = JSON.parse(arr);
-  arr.push(obj);
+  arr = addToArr(arr,obj)
   arr = JSON.stringify(arr);
   localStorage.setItem(key, arr);
 }
 function removeItemFrom(key, id) {
   let arr = localStorage.getItem(key);
   arr = JSON.parse(arr);
-  arr.forEach((e, index) => {
-    if (e.id === id) {
-      arr.splice(index, 1);
-    }
-  });
+  arr = removeFromArr(arr,id)
   arr = JSON.stringify(arr);
   localStorage.setItem(key, arr);
+}
+
+module.exports = {
+  addToArr ,
+  removeFromArr , 
+
 }
